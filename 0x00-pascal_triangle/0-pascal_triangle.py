cf5135ed8d9c2383fb5832f91fe4a4f6069d4d9d
Ezinne <ekalu.fk@gmail.com>
2023-09-28 16:11:38 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if triangle:
+            prev_row = triangle[-1]
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
